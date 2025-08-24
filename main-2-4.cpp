bf815468e393fc4c07fe31bf79f47557a05908e8
OOP2025 <3031760050@qq.com>
2025-08-24 22:11:16 +0930

    Delete main-2-4.cpp

--- main-2-4.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-extern int sum_min_max(int integers[], int);
-int main(){
-    int integers[]={1,2,3,2,1};
-    int length=5;
-    cout<<sum_min_max(integers,length)<<endl;
-    return 0;
-}
